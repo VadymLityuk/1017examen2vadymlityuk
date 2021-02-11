31247428a21de198438dbecf92ca519c06e802cf
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2021-02-10 19:19:21 -0500

    mes composant  aves routes et style

--- examen2/src/App.vue
+++ examen2/src/App.vue
@@ -3,9 +3,11 @@
   
     <Header/>
      <nav>
-      <router-link v-for="(item, i) in menuItems" :key="i" :to="item.path">{{ item.name }}</router-link>
+      <router-link v-for="(item, i) in menuItems" :key="i" :to="item.path">{{ item.name }}
+
+      </router-link>
     </nav>
-      <Accueil/>
+      
      
     <router-view />
        <Footer/>
@@ -16,12 +18,12 @@
 
 <script>
 //import MainPage from './components/MainPage.vue'
-import Accueil from './views/Accueil.vue';
+
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
 export default {
   name: 'App',
-  components: {Accueil,Header,Footer
+  components: {Header,Footer
   },
   data() {
     return {
--- examen2/src/views/Contact.vue
+++ examen2/src/views/Contact.vue
@@ -4,7 +4,7 @@
     <input type="text" name="lastname" placeholder="Nom" />
     <input type="text" name="firstname" placeholder="Prenom" />
     <input type="text" name="email" placeholder="Email" />
-    <select name="categorie">
+    <select class="sel" name="categorie">
         <option v-for="(subject, i) in dropdown" :key="i"> {{ subject }}</option>
     </select>
   </form>
@@ -26,10 +26,91 @@ export default {
 </script>
 
 <style scoped>
-.contact {
-    background-color: rgb(78, 78, 78);
-    padding: 2rem;
-    margin: 2rem 0;
-    text-align: left;
+
+
+h2 {
+  
+    font-family: Tomorrow, sans-serif;
+     background-color: var(--colors-primary--700);
+  color: var(--colors-on_primary--500);
+  font-size: .95rem;
+  display: inline-block;
+  padding: 0.125em 0.75em;
+  padding-right: 2em;
+  letter-spacing: 0.065em;
+  line-height: 1;
+  text-transform: uppercase;
+  clip-path: polygon(
+    0.25em 0,
+    calc(100% - 1em) 0,
+    100% 100%,
+    0.33em 100%,
+    0 100%,
+    0 0.25em
+  );
 }
+    form {
+       width: 30%;
+    height: 100%;
+    padding: 30px;
+    border: 2px var(
+        --colors-primary--600);
+    margin: 10% 35%;
+
+  
+  }
+  form .control{
+
+    background-color: var(--colors-bg--300);
+  border: 1px solid var(--colors-primary--600);
+  clip-path: var(--ui-notch-path);
+  padding-right: 0.5rem;
+  }
+  
+ 
+  
+  input {
+   background-color: transparent;
+  border: 0;
+  box-shadow: none;
+  color: var(--colors-primary--500);
+  font-family: var(--fonts-primary);
+  font-size: 1rem;
+  letter-spacing: 0.045em;
+  outline: none;
+  padding: 0.5rem;
+  resize: none;
+  width: 100%;
+    }
+    
+  input[type="text"] {
+     background-color: var(--colors-bg--300);
+  border: 1px solid var(--colors-primary--600);
+  clip-path: var(--ui-notch-path);
+  padding-right: 0.5rem;
+  }
+  
+  ::-webkit-scrollbar-thumb {
+    background: #000;
+  }
+
+  .sel{
+     font-family: Tomorrow, sans-serif;
+     background-color: var(--colors-primary--700);
+  color: var(--colors-on_primary--500);
+  display: inline-block;
+  text-transform: uppercase;
+  clip-path: polygon(
+    0.25em 0,
+    calc(100% - 1em) 0,
+    100% 100%,
+    0.33em 100%,
+    0 100%,
+    0 0.25em
+  );
+    border: 1px solid #473939 ;
+    border-bottom: 1px solid #473939;
+    text-transform: uppercase;
+  }
+  
 </style>
