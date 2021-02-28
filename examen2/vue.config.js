00a461bc8c4ea0e23667326a1f5f4146019f6de0
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2021-02-28 15:06:01 -0500

    ProjetFinal

--- examen2/vue.config.js
+++ examen2/vue.config.js
@@ -2,7 +2,6 @@
 
 /**
  // @type {import('@vue/cli-service').ProjectOptions}
- */
-module.exports = {
-   publicPath: "1017examen2vadymlityuk/examen2/dist/",
-  }
+ */ functionmodule.exports = {
+  publicPath: "1017examen2vadymlityuk/examen2/dist/",
+ }
