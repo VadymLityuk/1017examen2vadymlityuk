5c3c1a11dd303246d436b65a9014cce75b89345c
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2021-02-28 15:19:41 -0500

    ProjetFinale test mixin

---
+++ examen2/test/mixin.spec.js
@@ -0,0 +1,14 @@
+import mixinTranslations from '@/mixins/mixinTranslations';
+import { mount } from '@vue/test-utils';
+//translation test to jest
+describe('mtesting mixin translations', () => {
+    test("title propriety test", () => {
+      const Component = {
+        render() {},
+        title: 'translation mixin',
+        mixins: [mixinTranslations]
+      }
+      mount(Component)
+      expect(document.title).toBe('translation mixin')
+    })
+  })
