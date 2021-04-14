50ba1ac9cd00fd64e0860f4a31fe86429e87ca3c
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2021-04-13 21:09:09 -0400

    vue test pages + jest

---
+++ examen2/src/components/22.vue
@@ -0,0 +1,28 @@
+<!--MainPage.vue/ Page pour test jest-->
+<!--Vadym Lityuk 25 février 2021-->
+
+
+<template>
+  <div class="main-page">
+    <h1>
+     Test
+    </h1>
+    <button type="button" class="btn btn-outline-light" @click="$router.push({ name: 'accueil' })">Test Page</button>
+  </div>
+</template>
+
+<style lang="css" scoped>
+  .main-page {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 90vh;
+  }
+    h1 {
+      color: #ffffff;
+      font-size: 70px;
+      margin-bottom: 20px;
+    }
+  
+</style>
