1500d2cad14d0e7ca44e1e52421f79fcfd99e1d6
VadymLityuk <57510267+VadymLityuk@users.noreply.github.com>
2021-05-17 18:33:39 -0400

    too vue updatet

--- examen2/src/components/Footer.vue
+++ examen2/src/components/Footer.vue
@@ -3,7 +3,7 @@
 
 <template>
   <footer>
-     <div v-mydirective> - New update vue test-
+     <div v-mydirective> -to vue updatet-
        <a href="https://github.com/VadymLityuk/1017examen2vadymlityuk/blob/main/LICENSE">License</a>
         </div>
   </footer>
